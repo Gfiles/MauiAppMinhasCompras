dbbff01fdbb41e8f6096af624cb212d5abe3eb6e
Gfiles <gfilesg@gmail.com>
2025-03-05 00:57:11 -0300

    Inserção de Produto no Base de dados

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,7 +1,22 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    _db = new SQLiteDatabaseHelper(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "MinhasCompras.db3"));
+                }
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -6,4 +6,26 @@ public partial class ListaProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Somar_Clicked(object sender, EventArgs e)
+    {
+        try {
+            Navigation.PushAsync(new Views.NovoProduto());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Error", ex.Message, "Ok");
+        }
+    }
+
+    private void Adicionar_Clicked(object sender, EventArgs e)
+    {
+        try {
+            Navigation.PushAsync(new Views.NovoProduto());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Error", ex.Message, "Ok");
+        }
+    }
 }
