e137fa9ad837576e680d88b063a51a79250d2a81
Gfiles <gfilesg@gmail.com>
2025-03-16 03:02:18 -0300

    CreateSearchbar

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,21 +1,31 @@
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+    ObservableCollection<Produto> lista = new ObservableCollection<Produto>(); 
 	public ListaProduto()
 	{
 		InitializeComponent();
+
+        lst_produtos.ItemsSource = lista;
 	}
 
+    protected async override void OnAppearing()
+    {
+        List<Produto> tmp = await App.Db.GetAll();
+
+        tmp.ForEach( x => lista.Add(x));
+    }
     private void Somar_Clicked(object sender, EventArgs e)
     {
-        try {
-            Navigation.PushAsync(new Views.NovoProduto());
-        }
-        catch (Exception ex)
-        {
-            DisplayAlert("Error", ex.Message, "Ok");
-        }
+        double soma = lista.Sum(i => i.Total);
+
+        string msg = $"O total é {soma:C}";
+
+        DisplayAlert("Total dos Produtos", msg, "OK");
     }
 
     private void Adicionar_Clicked(object sender, EventArgs e)
@@ -27,5 +37,21 @@ public partial class ListaProduto : ContentPage
         {
             DisplayAlert("Error", ex.Message, "Ok");
         }
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string q = e.NewTextValue;
+
+        lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(x => lista.Add(x));
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+
     }
 }
